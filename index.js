d5944393de4725b5d93916d858f9f964ce613ad7
Danilo Augusto <127322985+dandanilo2010@users.noreply.github.com>
2023-10-24 13:17:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+let botao = document.getElementById('botao-adicionar')
+let input = document.getElementById('input-principal')
+let listaCompleta = document.getElementById('tarefas')
+
+let arrayDeTarefas = []
+
+function mostrarNaTela(){
+    let novaLista =""
+
+    arrayDeTarefas.forEach( tarefa => {
+        novaLista = novaLista + `
+        <li class="item-tarefa">
+        <p>${tarefa}</p>
+        </li>
+        `
+    })
+
+listaCompleta.innerHTML = novaLista
+
+}
+
+
+function cliqueiNoBotao(){
+    arrayDeTarefas.push(input.value)
+    mostrarNaTela()
+}
+
+
+botao.addEventListener("click", cliqueiNoBotao)
